cb81648db26b31a5c8475f1838a9f6bd3b74ee7a
loucas.kreger <loucas.kreger@moviri.com>
2025-08-04 13:40:36 -0400

    Fix issue with IMPORT_ALL_METRICS

--- func.py
+++ func.py
@@ -27,60 +27,62 @@ def process_metrics(body: Dict):
 
     datapoints = body.get("datapoints")
 
-    metric_map = namespace_map.get(namespace)
-    if metric_map is None:
-        logging.getLogger().error(
-            f"Could not find a metric mapping for namespace '{namespace}'"
-        )
-        return
-
     import_all_metrics = True if os.environ["IMPORT_ALL_METRICS"] == "True" else False
     logging.getLogger().info(f"import_all_metrics: {import_all_metrics}")
 
-    value_or_none = metric_map.value_from_oci_metric_name(
-        metric_name, oci_dimensions, datapoints
-    )
-    if value_or_none is None:
-        if import_all_metrics:
-            key_namespace = namespace.replace("oci_", "")
-            key = f"cloud.oci.{key_namespace}.{metric_name}"
+    if import_all_metrics:
+        key_namespace = namespace.replace("oci_", "")
+        key = f"cloud.oci.{key_namespace}.{metric_name}"
 
-            buckets = create_minutely_buckets(datapoints)
-            aggregated = {}
-            for timestamp, values in sorted(buckets.items()):
-                aggregated[timestamp] = SummaryStat(min(values), max(values), sum(values), len(values))
+        buckets = create_minutely_buckets(datapoints)
+        aggregated = {}
+        for timestamp, values in sorted(buckets.items()):
+            aggregated[timestamp] = SummaryStat(
+                min(values), max(values), sum(values), len(values)
+            )
 
-            for timestamp, summary_stat in aggregated.items():
+        for timestamp, summary_stat in aggregated.items():
+            mint_metric = MintMetric(
+                key,
+                summary_stat,
+                {
+                    "oci.resource_group": oci_dimensions.get("resourceGroup"),
+                    "oci.compartment_id": oci_dimensions.get("compartmentId"),
+                },
+                timestamp * 1000,
+            )
+            logging.getLogger().info(f"mint_metric: {mint_metric}")
+            push_metrics_to_dynatrace(mint_metric)
+    else:
+        metric_map = namespace_map.get(namespace)
+        if metric_map is None:
+            logging.getLogger().error(
+                f"Could not find a metric mapping for namespace '{namespace}'"
+            )
+            return
+        if value_or_none := metric_map.value_from_oci_metric_name(
+            metric_name, oci_dimensions, datapoints
+        ):
+            dynatrace_metric_key, results = value_or_none
+            dimensions = metric_map.dimensions(oci_dimensions)
+
+            for result in results:
                 mint_metric = MintMetric(
-                    key,
-                    summary_stat,
-                    {
-                        "oci.resource_group": oci_dimensions.get("resourceGroup"),
-                        "oci.compartment_id": oci_dimensions.get("compartmentId"),
-                    },
-                    timestamp * 1000,
+                    dynatrace_metric_key,
+                    result.value,
+                    dimensions,
+                    result.timestamp * 1000,
                 )
-                logging.getLogger().info(f"mint_metric: {mint_metric}")
-                push_metrics_to_dynatrace(mint_metric)
+            logging.getLogger().info(f"process_metrics: Mint Metric: {mint_metric}")
+            push_metrics_to_dynatrace(mint_metric)
         else:
             logging.getLogger().debug(
                 f"Could not find a mapping for metric '{metric_name}' in namespace '{namespace}'"
             )
-
-        return
-
-    dynatrace_metric_key, results = value_or_none
-    dimensions = metric_map.dimensions(oci_dimensions)
-
-    for result in results:
-        mint_metric = MintMetric(
-            dynatrace_metric_key, result.value, dimensions, result.timestamp * 1000
-        )
-    logging.getLogger().info(f"process_metrics: Mint Metric: {mint_metric}")
-    push_metrics_to_dynatrace(mint_metric)
-
+            return
 
 METRIC_INGEST_ENDPOINT = "/api/v2/metrics/ingest"
+
 
 def push_metrics_to_dynatrace(mint_metric: MintMetric):
     try:
@@ -100,7 +102,9 @@ def push_metrics_to_dynatrace(mint_metric: MintMetric):
             api_token = os.environ["DYNATRACE_API_KEY"]
             client.using_api_token(api_token)
         else:
-            logging.getLogger().error(f"Invalid authentication method '{auth_method}'. Expected either 'oauth' or 'token'")
+            logging.getLogger().error(
+                f"Invalid authentication method '{auth_method}'. Expected either 'oauth' or 'token'"
+            )
 
         proxy_url = create_proxy_connection()
         proxies = {"http": proxy_url, "https": proxy_url} if proxy_url else None
@@ -108,6 +112,7 @@ def push_metrics_to_dynatrace(mint_metric: MintMetric):
         client.send_mint_metric(str(mint_metric), proxies)
     except (Exception, ValueError) as ex:
         logging.getLogger().error(str(ex))
+
 
 def validate_connection():
     try:
@@ -120,14 +125,21 @@ def validate_connection():
             proxy_address = os.environ.get("PROXY_URL", None)
             logging.getLogger().info(f"Testing connection to proxy '{proxy_address}'")
             proxy_response = requests.head(proxy_address, timeout=5)
-            logging.getLogger().info(f"Validated connection to proxy and got ({proxy_response.status_code}): {proxy_response.text}")
+            logging.getLogger().info(
+                f"Validated connection to proxy and got ({proxy_response.status_code}): {proxy_response.text}"
+            )
 
         proxies = {"http": proxy_url, "https": proxy_url} if proxy_url else None
         logging.getLogger().info(f"Validating connection with proxies '{proxies}'")
         response = requests.head(tenant_url, proxies=proxies, timeout=5)
-        logging.getLogger().info(f"Validated connection and got ({response.status_code}): {response.text}")
+        logging.getLogger().info(
+            f"Validated connection and got ({response.status_code}): {response.text}"
+        )
     except (Exception, ValueError) as ex:
-        logging.getLogger().error(f"Error validating connection ({response.status_code}): {ex}")
+        logging.getLogger().error(
+            f"Error validating connection ({response.status_code}): {ex}"
+        )
+
 
 def create_proxy_connection() -> Optional[Dict[str, str]]:
     proxy_address = os.environ.get("PROXY_URL", None)
@@ -162,7 +174,7 @@ def _create_user_pass_url(proxy_username: str, proxy_password: str) -> str:
 def handler(ctx, data: io.BytesIO = None):
     log_level = str(os.environ["LOG_LEVEL"])
     logging.getLogger().setLevel(log_level.upper())
-    
+
     try:
         body = json.loads(data.getvalue())
         if isinstance(body, list):
